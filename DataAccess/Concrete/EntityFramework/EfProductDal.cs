b3d67a2665a0525a1b2c4187610362a84c618ddc
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-09-27 13:40:44 +0200

    Generated EfEntityRepositoryBase

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -3,61 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, ToyWorldContext>, IProductDal
     {
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (ToyWorldContext context = new ToyWorldContext())
-            {
-                return filter == null
-                    ? context.Set<Product>().ToList()
-                    : context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter = null)
-        {
-            using (ToyWorldContext context = new ToyWorldContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public void Add(Product entity)
-        {
-            using (ToyWorldContext context = new ToyWorldContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (ToyWorldContext context = new ToyWorldContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (ToyWorldContext context = new ToyWorldContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
     }
 }

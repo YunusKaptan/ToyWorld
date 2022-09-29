81802c5ba67fcad30f6586ba1e10434ddcfaced7
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-09-29 15:40:45 +0200

    Created WebAPI layer

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product {ProductId = 1, ProductName = "Elma"}
+            };
+        }
+    }
+}

ca303dbee86da9664013a9203f0f25d259dca40c
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-11-19 16:46:30 +0100

    Order Detail dto property created

---
+++ Entities/DTOs/OrderDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class OrderDetailDto
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerLastname { get; set; }
+        public DateTime OrderDate { get; set; }
+
+    }
+}
